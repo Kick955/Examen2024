4a2a26cb9f767705e66aaa566240379f19907c03
Kick955 <killianfortier.fr@gmail.com>
2024-06-05 13:11:19 +0200

    creer etudiant

--- src/packages/Etudiant.java
+++ src/packages/Etudiant.java
@@ -1,10 +1,28 @@
 package packages;
 
-
 class Etudiant extends Individu {
-    public Etudiant(String nom, String prenom, int identifiant) {
-		super(nom, prenom, identifiant);
+    public Etudiant(String Nom, String Prenom, int Identifiant) {
+		super(Nom, Prenom, Identifiant);
 		
+	    public Date getDateInscription() {
+	        return dateInscription;
+	    }
+
+	    public void setDateInscription(Date dateInscription) {
+	        this.dateInscription = dateInscription;
+	    }
+
+	    public String getNiveauEtude() {
+	        return niveauEtude;
+	    }
+
+	    public void setNiveauEtude(String niveauEtude) {
+	        this.niveauEtude = niveauEtude;
+	    }
+	   
+	    // Methode qui affiche les informations du cours
+	    public void montrerDetails() {
+	        System.out.println("Titre: " + getTitre() + "Formateur " + getFormateur() + ", Duree H" + getDuree() +
+	                ", placesRestantes: " + placesRestantes);
+	    }
 	}
-    
-}
