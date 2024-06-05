7ac04b26adc90ddf2dc796a1e3619e0402de7dcf
Kick955 <150357913+Kick955@users.noreply.github.com>
2024-06-05 13:19:47 +0200

    Main/Cours.java
    Ajouter le main et cours

--- src/packages/Cours.java
+++ src/packages/Cours.java
@@ -36,4 +36,18 @@ public int setplacesRestantes()
 this.placesRestantes = placesRestantes;
 }
 }
+ Fichier muain
 
+package packages;
+
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        // Création des étudiants
+        Etudiant etudiant = new Etudiant("Fortier", "Killian", 1, new Date(), "Licence");
+        
+        etudiant.montrerDetails();
+
+    }
+}
