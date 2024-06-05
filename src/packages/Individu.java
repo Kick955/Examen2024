6c2cb18d028745644285702b59e396cc79f24823
Kick955 <killianfortier.fr@gmail.com>
2024-06-05 13:13:20 +0200

    exo 1.2.3

--- src/packages/Individu.java
+++ src/packages/Individu.java
@@ -1,13 +1,48 @@
 package packages;
 
+//Classe Abstract
 abstract class Individu {
     private String Nom;
     private String Prenom;
     private int Identifiant;
 
-    public Individu(String Nom, String Prenom, int Identifiant) {
+//Classe Individu
+public Individu(String Nom, String Prenom, int Identifiant) {
         this.Nom = Nom;
         this.Prenom = Prenom;
         this.Identifiant = Identifiant;
     }
+}
+//Creer les getters 
+
+public String getNom() {
+return Nom;
+}
+    
+public String getPrenom() {
+return Prenom;
+}
+
+public int GetIdentifiant() {
+return Identifiant;
+}
+
+//Creer les setters
+
+public String setNom() {
+this.Nom = Nom; 
+}
+
+public String setPrenom() {
+this.Prenom = Prenom;
+}
+
+public int setIdentifiant() {
+this.Identifiant = Identifiant;
+}
+
+public abstract void montrerDetails());
+System.out.println("Etudiant: " + getNom() + " " + getPrenom() + ", ID: " + getIdentifiant() +
+        ", Date d'inscription: " + dateInscription + ", Niveau d'étude: " + niveauEtude);
+}
 }
